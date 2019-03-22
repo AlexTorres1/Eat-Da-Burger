ea57502e7550b66b85c3d190c4dffbbb7b5b773c
torn75 <torn_75@yahoo.com>
2019-03-22 19:26:03 -0400

    added files

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,12 @@ CREATE database burgers_db;
 
 USE burgers_db;
 
+DROP TABLE IF EXISTS burgers;
+
 CREATE TABLE burgers (
   id INT NOT NULL,
-  burger_name VARCHAR(100) NOT NULL,
-  devoured BOOLEAN NULL,
+  burger_name VARCHAR(20) NOT NULL,
+  devoured BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 );
 
--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 //Dependencies
 var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on port: " + PORT);
+  });
