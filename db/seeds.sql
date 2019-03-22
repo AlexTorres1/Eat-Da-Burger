4af54d07454e391ade0cc8d3592c39fa49082279
torn75 <torn_75@yahoo.com>
2019-03-22 19:25:12 -0400

    added files

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beef Burgers', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('50/50 burger', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Pepper Jack  Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Angus burger', true);
